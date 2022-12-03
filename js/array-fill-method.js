26b08215fde0a7a3991bb61559c61cec29898926
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-03 17:06:46 +0530

    Built-in-method

---
+++ js/array-fill-method.js
@@ -0,0 +1,14 @@
+var a = [1,2,22,3,4,5]
+
+var b = a.fill(10,2,5)
+// in this we are telling him with the 3 parameter starting is fill the value name 10 from 2 to 5 length
+
+document.write(b + "<br>")
+
+// we can do this with 2 parameter like that
+
+var c = a.fill(30,1)
+
+document.write(c)
+
+// this will telling us the start point but not telling us ending point so this will run still last value this is showing us to write 30 and start with 1
