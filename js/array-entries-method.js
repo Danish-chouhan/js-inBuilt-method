3c803ed343703fcfbd5c5c00832fa5efeace3880
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-04 00:33:08 +0530

    Built-in-method

---
+++ js/array-entries-method.js
@@ -0,0 +1,9 @@
+var entries = ["hlw","i","am","Danish","chouhan"]
+
+var calling = entries.entries()
+
+document.write(calling.next().value + "<br>")
+document.write(calling.next().value + "<br>")
+document.write(calling.next().value + "<br>")
+document.write(calling.next().value + "<br>")
+document.write(calling.next().value + "<br>")
--- js/array-findLastIndex-method.js
+++ js/array-findLastIndex-method.js
@@ -1,8 +1,11 @@
-var a = [11,22,33,44,55,66,77,88,99,100]
+// var a = [11,22,33,44,55,66,77,88,99,100]
 
-var b = a.findLastIndex(Danish)
+// var b = a.findLastIndex(Danish)
 
-function Danish(chouhan){
-    return chouhan < 111
-}
-document.write(b)
+// function Danish(chouhan){
+//     return chouhan < 111
+// }
+// document.write(b)
+
+const fn = fname => fname 
+console.log(fn("Shanwaz"))
