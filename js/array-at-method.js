1959c33f75ecdf834b11d33a6ef2c68e203ca420
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-03 16:12:37 +0530

    Built-in-method

---
+++ js/array-at-method.js
@@ -0,0 +1,6 @@
+var a = [1,2,10,4,5]
+
+var b = a.at(3)
+// this parameter is we called index always we write here a index value in this we used a index of 3 soo this will reeturn us a 4 becouse on the 3rd index the value is 4
+
+document.write(b)
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 [js-array-length](./js/array-lenth-method.js)
 ---
+[array-at-method](./js/array-at-method.js)
+---
