0f31c312e243b7d9c8e4ded384bab02e89b4b8d0
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-08 14:23:13 +0530

    Built-in-method

---
+++ String/js/string-padStart-Method.js
@@ -0,0 +1,13 @@
+var a = "Danish"
+var b = a.padStart(10,".")
+
+document.write(b + "<br>")
+
+var c = '200'
+var d = c.padStart(10,">")
+
+document.write(d)
+
+// Syntax
+// padStart(targetLength)
+// padStart(targetLength, padString)
