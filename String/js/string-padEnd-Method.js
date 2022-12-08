c4f6c86c2453e5495d78a895e77df7e601daa4bf
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-08 14:19:33 +0530

    Built-in-method

---
+++ String/js/string-padEnd-Method.js
@@ -0,0 +1,13 @@
+var a = "Danish"
+var b = a.padEnd(10,".")
+
+document.write(b + "<br>")
+
+var c = '200'
+var d = c.padEnd(10,">")
+
+document.write(d)
+
+// Syntax
+// padEnd(targetLength)
+// padEnd(targetLength, padString)
