8a6c3c9e5c1f24ecbcb081dd2e6a498564258ef5
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-08 16:00:54 +0530

    Built-in-method

---
+++ String/js/string-normalizastion-Method.js
@@ -0,0 +1,15 @@
+var a = "Danish"
+var b = "chouhan"
+var c = a.normalize()
+var d = b.normalize()
+document.write(c + " ")
+document.write(d + "<br>")
+
+document.write((c === d) + "<br>")
+document.write(c.length === d.length)
+
+
+
+// Syntax
+// normalize()
+// normalize(form)
