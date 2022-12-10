114cb0c50d399761382b13df564a2063244e40cd
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-10 17:01:44 +0530

    Built-in-method

---
+++ Object/js/Object-from-Entries-Method.js
@@ -0,0 +1,8 @@
+var a = new Map([
+    ['Danish','chouhan'],
+    ['and age is',17]
+]);
+var b = Object.fromEntries(a);
+console.log(b)
+
+// in this you may see that we maked an array but if we want this in object this is the method in this every first paremeter will be a properties of object 
