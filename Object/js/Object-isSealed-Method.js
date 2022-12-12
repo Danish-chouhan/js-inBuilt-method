e7df44c206768e8166054348385d99a958872846
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-13 01:22:01 +0530

    Built-in-method

---
+++ Object/js/Object-isSealed-Method.js
@@ -0,0 +1,7 @@
+var a = {
+    a : 17
+}
+Object.seal(a)
+document.write(Object.isSealed(a))
+
+//in this method if object is seal then this will show you true and if its not sealed then its show you false
