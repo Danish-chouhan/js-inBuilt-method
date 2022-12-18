feacb07dcb7d863d661bbf9d57f2a67dbb1af5f3
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-18 12:01:40 +0530

    in-bulit-methods

--- Object/js/testing-Object.js
+++ Object/js/testing-Object.js
@@ -7,17 +7,25 @@
 // b.name = "Danish-chouhan";
 // b.danish()
 
-var a = {};
-Object.defineProperties(a ,{
-    p1 : {
-        value : 17
-    },
-    p2 : {
-        value : 18
-    },
-    p3 : {
-        value : 19
-    }
-});
+// var a = {};
+// Object.defineProperties(a ,{
+//     p1 : {
+//         value : 17
+//     },
+//     p2 : {
+//         value : 18
+//     },
+//     p3 : {
+//         value : 19
+//     }
+// });
 
-document.write(a.p3)
+// document.write(a.p3)
+
+var a = {}
+Object.defineProperty(a , 'p1' , {
+    value : 17
+});
+a.p1 = 118
+var b = a.p1
+document.write(b)
