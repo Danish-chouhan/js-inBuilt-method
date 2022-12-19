472b7b73c3692e274646f4c21bfd60879b2b9bdd
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-19 11:21:49 +0530

    in-bulit-methods

--- Array/js/Test-for-practice.js
+++ Array/js/Test-for-practice.js
@@ -1,5 +1,9 @@
 // var a = ["Danish chouhan" , 17]
 // document.write(a.length)
 
-var a =[1,2,3,4,5];
-document.write(a.at(4))
+// var a =[1,2,3,4,5];
+// document.write(a.at(4));
+
+var a = [1,2,3];
+var b = [4,5,6];
+document.write(a.concat(b))
