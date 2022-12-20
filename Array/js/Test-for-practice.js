c70ca4df04d0ca15845871258715dc85cefb7a54
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-20 19:32:15 +0530

    Built-In-method

--- Array/js/Test-for-practice.js
+++ Array/js/Test-for-practice.js
@@ -4,6 +4,9 @@
 // var a = ['danish',"chouhan"]
 // document.write(a.at(1))
 
+// var a = [1,2,3,4,5]
+// var b = [6,7,8,9,10]
+// document.write(a.concat(b))
+
 var a = [1,2,3,4,5]
-var b = [6,7,8,9,10]
-document.write(a.concat(b))
+document.write(a.copyWithin(2,1,5))
