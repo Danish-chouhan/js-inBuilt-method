b62c45a55f631059940085b6956b8fbc22950d88
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-20 19:34:02 +0530

    Built-In-method

--- Array/js/Test-for-practice.js
+++ Array/js/Test-for-practice.js
@@ -8,5 +8,10 @@
 // var b = [6,7,8,9,10]
 // document.write(a.concat(b))
 
-var a = [1,2,3,4,5]
-document.write(a.copyWithin(2,1,5))
+// var a = [1,2,3,4,5]
+// document.write(a.copyWithin(2,1,5))
+
+var a = ["a","b","c","d"]
+var b = a.entries()
+document.write(b.next().value)
+document.write(b.next().value)
