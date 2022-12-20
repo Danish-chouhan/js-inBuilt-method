5662d5136d36518de71ed78e919fdbff5f6654fc
Danish-chouhan <danishchouhan41@gmail.com>
2022-12-20 19:35:37 +0530

    Built-In-method

--- Array/js/Test-for-practice.js
+++ Array/js/Test-for-practice.js
@@ -11,7 +11,14 @@
 // var a = [1,2,3,4,5]
 // document.write(a.copyWithin(2,1,5))
 
-var a = ["a","b","c","d"]
-var b = a.entries()
-document.write(b.next().value)
-document.write(b.next().value)
+// var a = ["a","b","c","d"]
+// var b = a.entries()
+// document.write(b.next().value)
+// document.write(b.next().value)
+
+var a = [1,2,3,4,5,6,7,8,9,10]
+var b = a.every(dan)
+function dan(x){
+    return x < 11
+}
+document.write(b)
